6b8504f23eeadd81194985aa48d05f6716eaf51a
win <muhaimin2547@gmail.com>
2023-02-15 00:36:23 +0700

    consoletum

--- Program.cs
+++ Program.cs
@@ -3,13 +3,20 @@
 namespace workshop2__1 {
     class Program {
         static void Main(string[] args) {
-            const double PI = 3.1415926535; 
-          double radius = 5.0; 
+             double number1 = 8.25;
+          double number2 = 1.23;
 
-          double area = PI * radius * radius;
-          double length = 2 * PI * radius;
+          double summary = number1 + number2;
+          double different = number1 - number2;
+          double multiply = number1 * number2;
+          double divide = number1 / number2;
 
-          Console.WriteLine("Ares is: {0}, Length: {1}", area, length);
+          Console.WriteLine("Number 1 is: {0}", number1);
+          Console.WriteLine("Number 2 is: {0}", number2);
+          Console.WriteLine("Summary == {0} + {1} = {2}", number1, number2, summary);
+          Console.WriteLine("Different == {0} - {1} = {2}", number1, number2, different);
+          Console.WriteLine("Multiply == {0} x {1} = {2}", number1, number2, multiply);
+          Console.WriteLine("Divide == {0} / {1} = {2}", number1, number2, divide);
 
         }
     }
