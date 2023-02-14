4017177a5afc0bf1b6a53b06e0589ac5bd0f06c2
win <muhaimin2547@gmail.com>
2023-02-15 01:00:16 +0700

    <>

--- Program.cs
+++ Program.cs
@@ -3,20 +3,11 @@
 namespace workshop2__1 {
     class Program {
         static void Main(string[] args) {
-             double number1 = 8.25;
-          double number2 = 1.23;
+            bool result1 = !(7 >= 2)&& (4 == 2) || (4 < 2);
+        bool result2 = (3 > 3) || ((5 > 2) && (4 < 1));
 
-          double summary = number1 + number2;
-          double different = number1 - number2;
-          double multiply = number1 * number2;
-          double divide = number1 / number2;
-
-          Console.WriteLine("Number 1 is: {0}", number1);
-          Console.WriteLine("Number 2 is: {0}", number2);
-          Console.WriteLine("Summary == {0} + {1} = {2}", number1, number2, summary);
-          Console.WriteLine("Different == {0} - {1} = {2}", number1, number2, different);
-          Console.WriteLine("Multiply == {0} x {1} = {2}", number1, number2, multiply);
-          Console.WriteLine("Divide == {0} / {1} = {2}", number1, number2, divide);
+        Console.WriteLine("Result 1 is: {0}", result1);
+        Console.WriteLine("Result 2 is: {0}", result2);
 
         }
     }
