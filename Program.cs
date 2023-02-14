11a821f1df0705a643283d924709987ae59ae0ae
win <muhaimin2547@gmail.com>
2023-02-14 23:33:04 +0700

    Iam

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace workshop2__1 {
+    class Program {
+        static void Main(string[] args) {
+             string name = "assawin sangmarn";
+          int age = 18;
+
+          Console.WriteLine("I am {0}. I am {1} years old", name, age);
+        }
+    }
+}
