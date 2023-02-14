103b27b1f36e84c877ab850744e489fd31ec035a
win <muhaimin2547@gmail.com>
2023-02-14 23:49:47 +0700

    3750

--- Program.cs
+++ Program.cs
@@ -3,14 +3,14 @@
 namespace workshop2__1 {
     class Program {
         static void Main(string[] args) {
-            int capital = 20000; 
+            int capital = 20000;  // setup capital
           int year = 3;
           float rate = 6.25f;
 
           float result = capital * year * rate / 100.0f;
 
           Console.WriteLine("result is {0}", result);
-    
+
         }
     }
 }
